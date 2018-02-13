1bdcad95075758daf2899f574fbae6701ec1504b
Bro4all <omar.habra@sjsu.edu>
2018-02-12 21:07:55 -0800

    2 tests only failing

--- lib/lab02/src/stringVector.cpp
+++ lib/lab02/src/stringVector.cpp
@@ -1,5 +1,6 @@
 #include "../inc/stringVector.h"
 #include <iostream>
+int stringCompare (std::string str1, std::string str2 );
 
 stringVector::stringVector() {
     data=nullptr;
@@ -28,7 +29,7 @@ void stringVector::reserve(unsigned new_size) {
         temp[i] = data[i];
     }
     delete[]data;
-    *data=*temp;
+    data=temp;
 
 
 }
@@ -64,14 +65,15 @@ void stringVector::append(std::string new_data) {
 
 
 void stringVector::swap(unsigned pos1, unsigned pos2) {
-    std::string tmstr; //atemporary string to hold the data and switch it
-    if ((pos1>=length)||(pos2>=length)){
-        std::cout<<"indec Out of bounds"<<std::endl;
+    std::string temp; //a temporary string to hold the data and switch it
+    if((pos1>=length)||(pos2>=length)){
+        std::cout<<"index Out of bounds"<<std::endl;
         return;
+    }{
+        temp = data[pos1];
+        data[pos1] = data[pos2];
+        data[pos2] = temp;
     }
-    tmstr=data[pos1];
-    data[pos1]=data[pos2];
-    data[pos2]=tmstr;
 
 }
 
@@ -84,14 +86,15 @@ stringVector &stringVector::operator=(stringVector const &rhs) {
     for (int i=0;i<length;i++){
         this->data[i]=rhs.data[i];
     }
+    return *this;
     //return ;
 }
 
 std::string &stringVector::operator[](unsigned position) {
-    if(position>length){
+    if(position>allocated_length){
         throw std::out_of_range("position out of range");}
 
-        return data[position-1];
+        return data[position];
     }
 
 
@@ -99,16 +102,24 @@ std::string &stringVector::operator[](unsigned position) {
 
 
 void stringVector::sort() {
-    std::string str;
-    for(int i=(length-1);i>0;i--){
-        for(int j=0;j<i;j++){
-            if(data[j].compare(data[j+1])>0){
-                str=data[j];
-                data[j+1]=str;
-
+    std::string temp;
+    for(int index = length - 1; index > 0; index--) {
+        for (int j = 0; j < index; j++) {
+            if(stringCompare(data[j], data[j+1]) > 0){
+                temp = data[j];
+                data[j] = data[j+1];
+                data[j+1] = temp;
             }
         }
     }
-
-
+}
+int stringCompare (std::string str1, std::string str2 ){
+    int pos;
+    for(pos = 0; pos < str1.length() && pos < str2.length() && tolower(str1[pos]) == tolower(str2[pos]);++pos);
+    if (str1.length() < str2.length() && pos == str1.length() )
+        return -1;
+    else if (str2.length() < str1.length() && pos == str2.length())
+        return 1;
+    else
+        return pos == str1.length() ? 0 :tolower(str1[pos]) - tolower(str2[pos]);
 }
