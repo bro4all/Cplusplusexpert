612a0181e70efcebbc4d9ad5f25e29008160d887
Bro4all <omar.habra@sjsu.edu>
2018-02-17 20:24:52 -0800

    finished 2 codes.

--- lib/lab03/src/fifo.cpp
+++ lib/lab03/src/fifo.cpp
@@ -2,42 +2,92 @@
 
 namespace lab3{
     fifo::fifo() {
-    //Reserve 100 spaces in fifo_storage
+        lab2::stringVector fifo_storage;
+        fifo_storage.reserve(100);
+        front_index=0;
+        back_index=0;
+        //Reserve 100 spaces in fifo_storage
     }
 
     fifo::fifo(std::string input_string) {
-
+        fifo_storage.reserve(100);
+        fifo_storage[0]=input_string;
+       front_index=0;
+        back_index=0;
     }
 
     fifo::fifo(const fifo &original) {
 
+        fifo_storage.reserve(original.fifo_storage.capacity());
+        front_index=original.front_index;
+        back_index=original.back_index;
+        for(int i=0;i<original.fifo_storage.size();i++){
+            fifo_storage[i]=original.fifo_storage[i];
+        }
     }
 
     fifo::~fifo() {
+        front_index=-1;
+        back_index=-1;
 
     }
 
     fifo &fifo::operator=(const fifo &right) {
-        //return <#initializer#>;
+        if (&right == this){
+            return (*this);
+        }
+        fifo_storage.reserve(right.fifo_storage.capacity());
+        front_index = right.front_index;
+        back_index = right.back_index;
+        for(int i = 0;i<right.fifo_storage.size();i++){
+            fifo_storage[i] = right.fifo_storage[i];
+        } //return <#initializer#>;
     }
 
     bool fifo::is_empty() const {
         //return false;
+        if(front_index == -1){
+            return true;
+        }
+        return false;
+
+        //return false;
     }
 
     unsigned fifo::size() const {
-        //return 0;
-    }
+            unsigned int temp;
+            if(front_index<back_index){
+                temp = back_index-front_index+1;
+            }
+            else{
+                //If back index < front index then the list has wrapped around
+                temp = (100-front_index)+back_index+1;
+                //fifo_storave.size() - front index = how many until end.
+                //Adding the back index +1 should be how many items are in that section
+            }
+            return (temp);
+        }
 
     std::string fifo::top() const {
         //return std::__cxx11::string();
+        return fifo_storage[front_index];
     }
 
     void fifo::enqueue(std::string input) {
+        fifo_storage[back_index]=input;
+        back_index++;
+        if(fifo_storage.capacity()<=back_index){
+            fifo_storage.reserve(fifo_storage.capacity()+20):
 
+        }
     }
 
     void fifo::dequeue() {
+        front_index++;
+        if(front_index==back_index){
+            front_index=-1;
+            back_index=-1;
+        }
 
     }
 }
--- lib/lab03/src/lifo.cpp
+++ lib/lab03/src/lifo.cpp
@@ -1,43 +1,83 @@
 #include "lifo.h"
 
-namespace lab3{
+namespace lab3 {
     lifo::lifo() {
-    //Reserve 100 spaces in lifo_storage
+        lifo_storage.reserve(100);
+        index = -1//Reserve 100 spaces in lifo_storage
     }
 
     lifo::lifo(std::string input_string) {
-
+        lifo_storage.reserve(100);
+        lifo_storage[0] = input_string;
+        index = 0;
     }
 
     lifo::lifo(const lifo &original) {
+        lifo_storage.reserve(original.lifo_storage.capacity());
+        index = original.index;
+        for (int i = 0; i <= index; i++) {
+            lifo_storage[i] = original.lifo_storage[i];
+        }
 
     }
 
     lifo::~lifo() {
+        index = -1;
 
     }
 
     lifo &lifo::operator=(const lifo &right) {
         //return <#initializer#>;
+        lifo_storage.reserve(right.lifo_storage.capacity());
+        index = right.index;
+        for (int i = 0; i <= index; i++) {
+            lifo_storage[i] = right.lifo_storage[i];
+        }
+
+        return *this;
     }
 
+
     bool lifo::is_empty() const {
+        if (index == -1) {
+            return true;
+        } else {
+            return false;
+        }
         //return false;
     }
 
     unsigned lifo::size() const {
-        //return 0;
+        int temp;
+        if(index>-1) {
+            for (int i = 0; i <= index; i++) {
+                temp++;
+            }
+        }
+        else{
+            temp=0;
+        }
+        return temp;
+
     }
+
+
+
 
     std::string lifo::top() const {
-        //return std::__cxx11::string();
+        return lifo_storage[index]; //return std::__cxx11::string();
     }
 
-    void lifo::push(std::string input) {
+    void lifo::push(std::string input) {if(index == lifo_storage.capacity()-1){
+            lifo_storage.reserve(lifo_storage.capacity() + 20);
+        }
+        lifo_storage[++index] = input;
 
     }
 
     void lifo::pop() {
-
+        if(index > -1)
+            index--;
+    }
     }
 }
