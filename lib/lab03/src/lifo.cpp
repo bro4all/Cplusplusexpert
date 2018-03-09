9cdfe6a1e6cb330568de905a1548e1b113e24499
Bro4all <omar.habra@sjsu.edu>
2018-03-09 00:57:29 -0800

    03/08/2017

--- lib/lab03/src/lifo.cpp
+++ lib/lab03/src/lifo.cpp
@@ -1,25 +1,25 @@
 #include "lifo.h"
 namespace lab3{
     lifo::lifo() {
-        lifo_storage.reserve(100);//You would need to reserve 100 spaces because of the readme
-        index=0;//Set the index of the array = to 0
+        lifo_storage.reserve(100);
+        index=0;
     }
     lifo::lifo(std::string input_string) {
         lifo_storage.reserve(100);
-        lifo_storage.append(input_string);// same as the first constructor but you are appending an input_string
+        lifo_storage.append(input_string);
         index=1;
     }
     lifo::lifo(const lifo &original) {
-        index = original.index;//Copies the information from the private to &original. had to add const to the lab2 std::string &operator[](unsigned position) in order for this to work.
+        index = original.index;
         for(int i=0; i<index;i++){
-            lifo_storage[i]=original.lifo_storage[i];// Have to do an array to copy each array element onto original
+            lifo_storage[i]=original.lifo_storage[i];
         }
     }
     lifo::~lifo() {
-        lifo_storage.set_size(0);// same as fifo, but you just need to make index =0
+        lifo_storage.set_size(0);
         index=0;
     }
-    lifo &lifo::operator=(const lifo &right) {// Same as fifo, assignment operator
+    lifo &lifo::operator=(const lifo &right) {
         lifo_storage.reserve(right.lifo_storage.capacity());
         index= right.index;
         for(int i=0; i<= index; i++){
@@ -27,7 +27,7 @@ namespace lab3{
         }
         //return <#initializer#>;
     }
-    bool lifo::is_empty() const {// It should return true unless the index is greater than or equal to 1
+    bool lifo::is_empty() const {
         if(index>=1){
             return false;
         }
@@ -36,7 +36,7 @@ namespace lab3{
         }
     }
     unsigned lifo::size() const {
-        if(is_empty()==false){ 
+        if(is_empty()==false){
             return index;
         }
         else {
